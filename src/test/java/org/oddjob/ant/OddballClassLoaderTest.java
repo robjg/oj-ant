561e197dc487d7622f491fe7658c9685903a5a04
Rob Gordon <rob@rgordon.co.uk>
2026-05-29 07:08:45 +0100

    Separate DefaultConversionRegistry from ConversionLookup.

--- src/test/java/org/oddjob/ant/OddballClassLoaderTest.java
+++ src/test/java/org/oddjob/ant/OddballClassLoaderTest.java
@@ -33,218 +33,216 @@ import static org.junit.jupiter.api.Assertions.*;
 @Tag("IntegrationTest")
 public class OddballClassLoaderTest {
 
-	private static final Logger logger = LoggerFactory.getLogger(
-			OddballClassLoaderTest.class);
-	
+    private static final Logger logger = LoggerFactory.getLogger(
+            OddballClassLoaderTest.class);
 
-	@BeforeEach
-	void setUp(TestInfo testInfo) {
-		logger.info("---------------  " + testInfo.getDisplayName() + " ---------------");
-		logger.info("stdout is " + System.out);
-	}
-	
+
+    @BeforeEach
+    void setUp(TestInfo testInfo) {
+        logger.info("---------------  " + testInfo.getDisplayName() + " ---------------");
+        logger.info("stdout is " + System.out);
+    }
+
     @AfterEach
-	void tearDown() {
+    void tearDown() {
 
-		logger.info("stdout is " + System.out);
-	}
-	
-    @Test
-	void testLoadOddball() throws Exception {
-		
-		Path oddjobApp = OddjobSrc.oddjobApp();
-
-		FilesType oddjobLib = new FilesType();
-		oddjobLib.setFiles(oddjobApp.toString() + "/lib/*.jar");
-		
-		File[] files = oddjobLib.toFiles();
-		assertTrue(files.length > 0);
-		
-		File[] all = new File[files.length + 1];
-		all[0] = OddjobSrc.appJar().toFile();
-
-		System.arraycopy(files, 0, all, 1, files.length);
-		
-		URLClassLoaderType loaderType = new URLClassLoaderType();
-		loaderType.setFiles(all);
-		loaderType.setParent(getClass().getClassLoader());
-		loaderType.configured();
-
-		Path oddballDir = OurDirs.buildDirPath()
-				.resolve("oddball");
-
-		Oddball oddball = DirectoryOddballFactory.from(oddballDir.toFile())
-				.createFrom(loaderType.toValue());
-	
-		ArooaDescriptor descriptor = oddball.getArooaDescriptor();
-		
-		ArooaClass cl = descriptor.getElementMappings(
-				).mappingFor(
-						new ArooaElement("ant"), 
-						new InstantiationContext(ArooaType.COMPONENT, null));
-		
-		assertNotNull(cl);
-	}
+        logger.info("stdout is " + System.out);
+    }
 
     @Test
-	public void testClassLoaderOfTasksCreated() 
-	throws Exception {
+    void testLoadOddball() throws Exception {
 
-		OurDirs base = new OurDirs();
+        Path oddjobApp = OddjobSrc.oddjobApp();
 
-		ClassLoader existing = Thread.currentThread().getContextClassLoader();
-		
-		Thread.currentThread().setContextClassLoader(null);
-		
-		try {
-			Path oddjobApp = OddjobSrc.oddjobApp();
-			
-			FilesType oddjobLib = new FilesType();
-			oddjobLib.setFiles(oddjobApp + "/lib/*.jar");
-			
-			File[] files = oddjobLib.toFiles();
-			assertTrue(files.length > 0);
-			
-			File[] all = new File[files.length + 2];
-			all[0] = OddjobSrc.appJar().toFile();
-			// needed for log4j to ensure we don't pick up a test version
-			// because System.out will already be capturing log messages from
-			// previous tests - we don't want these in our console capture.
-			all[1] = base.relative("test/launch");
-			System.arraycopy(files, 0, all, 2, files.length);
-			
-			URLClassLoaderType loaderType = new URLClassLoaderType();
-			loaderType.setFiles(all);
-			loaderType.setParent(ClassLoader.getPlatformClassLoader());
-			loaderType.configured();
-			
-			ClassLoader loader = loaderType.toValue();
-			
-			Class<?> ojClass = loader.loadClass(
-					Oddjob.class.getName());
+        FilesType oddjobLib = new FilesType();
+        oddjobLib.setFiles(oddjobApp.toString() + "/lib/*.jar");
 
-			Runnable oddjob = (Runnable) ojClass.getConstructor().newInstance();
-	
-			Object oddball = loader.loadClass(
-					OddballsDescriptorFactory.class.getName())
-					.getConstructor().newInstance();
-			
-			BeanUtilsPropertyAccessor accessor = 
-				new BeanUtilsPropertyAccessor();
+        File[] files = oddjobLib.toFiles();
+        assertTrue(files.length > 0);
 
-			File oddballDir = base.relative("target/oddball");
+        File[] all = new File[files.length + 1];
+        all[0] = OddjobSrc.appJar().toFile();
 
-			accessor.setProperty(oddball, "files", 
-					new File[] { oddballDir } );
-			accessor.setProperty(oddjob, "descriptorFactory", oddball);
-					
-			accessor.setProperty(oddjob, "file", 
-					base.relative("test/files/classloader-test.xml"));
-			
-			ConsoleCapture console = new ConsoleCapture();
-			try (ConsoleCapture.Close ignored = console.captureConsole()) {
-				
-				oddjob.run();		
-			}
-			
-			console.dump(logger);
-						
-			String[] lines = console.getLines();
-			
-			assertEquals(2, lines.length);
+        System.arraycopy(files, 0, all, 1, files.length);
 
-			
-			assertEquals("     [echo] Oddball ClassLoader", 
-					lines[0].substring(0, 31));
-			
-		}
-		finally {
-			Thread.currentThread().setContextClassLoader(existing);
-		}
-	}
-	
+        URLClassLoaderType loaderType = new URLClassLoaderType();
+        loaderType.setFiles(all);
+        loaderType.setParent(getClass().getClassLoader());
+        loaderType.configured();
+
+        Path oddballDir = OurDirs.buildDirPath()
+                .resolve("oddball");
+
+        Oddball oddball = DirectoryOddballFactory.from(oddballDir.toFile())
+                .createFrom(loaderType.toValue());
+
+        ArooaDescriptor descriptor = oddball.getArooaDescriptor();
+
+        ArooaClass cl = descriptor.getElementMappings(
+        ).mappingFor(
+                new ArooaElement("ant"),
+                new InstantiationContext(ArooaType.COMPONENT, null));
+
+        assertNotNull(cl);
+    }
+
     @Test
-	public void testClassLoaderOfSubProject() 
-	throws IOException, ArooaPropertyException, ArooaConversionException {
+    public void testClassLoaderOfTasksCreated()
+            throws Exception {
 
-		OurDirs base = new OurDirs();
+        OurDirs base = new OurDirs();
 
-		Path oddjobApp = OddjobSrc.oddjobApp();
+        ClassLoader existing = Thread.currentThread().getContextClassLoader();
 
-		FilesType oddjobLib = new FilesType();
-		oddjobLib.setFiles(oddjobApp + "/lib/*.jar");
-		
-		File[] files = oddjobLib.toFiles();
-		assertTrue(files.length > 0);
-		
-		URLClassLoaderType loaderType = new URLClassLoaderType();
-		loaderType.setFiles(files);
-		loaderType.setParent(getClass().getClassLoader());
-		loaderType.configured();
-		
-		Oddjob oddjob = new Oddjob();
-		oddjob.setClassLoader(loaderType.toValue());
+        Thread.currentThread().setContextClassLoader(null);
 
-		Path oddballDir = OurDirs.buildDirPath().resolve("oddball");
+        try {
+            Path oddjobApp = OddjobSrc.oddjobApp();
 
-		OddballsDescriptorFactory oddball = 
-			new OddballsDescriptorFactory();
-		oddball.setFiles(new File[] { oddballDir.toFile() });
-		oddjob.setDescriptorFactory(oddball);
+            FilesType oddjobLib = new FilesType();
+            oddjobLib.setFiles(oddjobApp + "/lib/*.jar");
 
-		String xml = 
-			"<oddjob>" +
-			" <job>" +
-			"  <ant id='myant'>" +
-			"   <output>" +
-			"    <identify id='ant-output'>" +
-			"     <value>" +
-			"      <buffer/>" +
-			"     </value>" +
-			"    </identify>" +
-			"   </output>" +
-			"   <tasks>" +
-			"    <xml>" +
-			"     <tasks>" +
-			"      <ant antfile='" + 
-						base.relative("test/files/AntCallsOddjobTaskWithTaskDef.xml") + "' " +
-						"dir='" + base.relative("test/files") + "'/>" +
-			"     </tasks>" +
-			"    </xml>" +
-			"   </tasks>" +
-			"  </ant>" +
-			" </job>" +
-			"</oddjob>";
-			
-		oddjob.setConfiguration(new XMLConfiguration("XML", xml));
-		
-		ConsoleCapture console = new ConsoleCapture();
-		try (ConsoleCapture.Close ignored = console.captureConsole()) {
-			
-			oddjob.run();
-		}
+            File[] files = oddjobLib.toFiles();
+            assertTrue(files.length > 0);
 
-		assertEquals(ParentState.COMPLETE, 
-				oddjob.lastStateEvent().getState());
-		
-		console.dump(logger);
-		
-		String buffer = new OddjobLookup(oddjob).lookup(
-				"ant-output", String.class);
-		
-		assertTrue(buffer.contains("COMPLETE"));
-		
-		ClassLoader classLoader = new OddjobLookup(oddjob).lookup(
-				"myant.class.classLoader", ClassLoader.class);
-		
-		
-		
-		String[] lines = console.getLines();
-		
-		assertEquals(1, lines.length);
-		
-		assertEquals(classLoader.toString(), lines[0]);
-	}
+            File[] all = new File[files.length + 2];
+            all[0] = OddjobSrc.appJar().toFile();
+            // needed for log4j to ensure we don't pick up a test version
+            // because System.out will already be capturing log messages from
+            // previous tests - we don't want these in our console capture.
+            all[1] = base.relative("test/launch");
+            System.arraycopy(files, 0, all, 2, files.length);
+
+            URLClassLoaderType loaderType = new URLClassLoaderType();
+            loaderType.setFiles(all);
+            loaderType.setParent(ClassLoader.getPlatformClassLoader());
+            loaderType.configured();
+
+            ClassLoader loader = loaderType.toValue();
+
+            Class<?> ojClass = loader.loadClass(
+                    Oddjob.class.getName());
+
+            Runnable oddjob = (Runnable) ojClass.getConstructor().newInstance();
+
+            Object oddball = loader.loadClass(
+                            OddballsDescriptorFactory.class.getName())
+                    .getConstructor().newInstance();
+
+            BeanUtilsPropertyAccessor accessor =
+                    new BeanUtilsPropertyAccessor();
+
+            File oddballDir = base.relative("target/oddball");
+
+            accessor.setProperty(oddball, "files",
+                    new File[]{oddballDir});
+            accessor.setProperty(oddjob, "descriptorFactory", oddball);
+
+            accessor.setProperty(oddjob, "file",
+                    base.relative("test/files/classloader-test.xml"));
+
+            ConsoleCapture console = new ConsoleCapture();
+            try (ConsoleCapture.Close ignored = console.captureConsole()) {
+
+                oddjob.run();
+            }
+
+            console.dump(logger);
+
+            String[] lines = console.getLines();
+
+            assertEquals(2, lines.length);
+
+
+            assertEquals("     [echo] Oddball ClassLoader",
+                    lines[0].substring(0, 31));
+
+        } finally {
+            Thread.currentThread().setContextClassLoader(existing);
+        }
+    }
+
+    @Test
+    public void testClassLoaderOfSubProject()
+            throws IOException, ArooaPropertyException, ArooaConversionException {
+
+        OurDirs base = new OurDirs();
+
+        Path oddjobApp = OddjobSrc.oddjobApp();
+
+        FilesType oddjobLib = new FilesType();
+        oddjobLib.setFiles(oddjobApp + "/lib/*.jar");
+
+        File[] files = oddjobLib.toFiles();
+        assertTrue(files.length > 0);
+
+        URLClassLoaderType loaderType = new URLClassLoaderType();
+        loaderType.setFiles(files);
+        loaderType.setParent(getClass().getClassLoader());
+        loaderType.configured();
+
+        Oddjob oddjob = new Oddjob();
+        oddjob.setClassLoader(loaderType.toValue());
+
+        Path oddballDir = OurDirs.buildDirPath().resolve("oddball");
+
+        OddballsDescriptorFactory oddball =
+                new OddballsDescriptorFactory();
+        oddball.setFiles(new File[]{oddballDir.toFile()});
+        oddjob.setDescriptorFactory(oddball);
+
+        String xml =
+                "<oddjob>" +
+                        " <job>" +
+                        "  <ant id='myant'>" +
+                        "   <output>" +
+                        "    <identify id='ant-output'>" +
+                        "     <value>" +
+                        "      <buffer/>" +
+                        "     </value>" +
+                        "    </identify>" +
+                        "   </output>" +
+                        "   <tasks>" +
+                        "    <xml>" +
+                        "     <tasks>" +
+                        "      <ant antfile='" +
+                        base.relative("test/files/AntCallsOddjobTaskWithTaskDef.xml") + "' " +
+                        "dir='" + base.relative("test/files") + "'/>" +
+                        "     </tasks>" +
+                        "    </xml>" +
+                        "   </tasks>" +
+                        "  </ant>" +
+                        " </job>" +
+                        "</oddjob>";
+
+        oddjob.setConfiguration(new XMLConfiguration("XML", xml));
+
+        ConsoleCapture console = new ConsoleCapture();
+        try (ConsoleCapture.Close ignored = console.captureConsole()) {
+
+            oddjob.run();
+        }
+
+        assertEquals(ParentState.COMPLETE,
+                oddjob.lastStateEvent().getState());
+
+        console.dump(logger);
+
+        String buffer = new OddjobLookup(oddjob).lookup(
+                "ant-output", String.class);
+
+        assertTrue(buffer.contains("COMPLETE"));
+
+        ClassLoader classLoader = new OddjobLookup(oddjob).lookup(
+                "myant.class.classLoader", ClassLoader.class);
+
+
+        String[] lines = console.getLines();
+
+        assertEquals(1, lines.length);
+
+        assertEquals(classLoader.toString(), lines[0]);
+    }
 
 
 }
